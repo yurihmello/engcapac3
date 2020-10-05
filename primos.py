17a0af1e5ce3d11a8752b1585d136ca9f83b3bc0
YURI H MELLO <you@example.com>
2020-10-05 20:29:04 -0300

    AC03devops

---
+++ primos.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+    limite = 100
+
+    c = 1
+    p = 1
+    numero = 3
+
+    primos = "2,"
+
+    while p< limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1
+
+    return primos
